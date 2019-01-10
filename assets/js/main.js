785fe8675c5e1518a2150cf3acb25a3988aca9fc
Quintessa <anderson.quintessa@gmail.com>
2019-01-09 19:48:03 -0600

    pushing code for hour and minute hands - now working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function setSeconds() {
+function setDate() {
+	//set the current date
 	const now = new Date();
 	const seconds = now.getSeconds();
-	const secondsDegrees = ((seconds/60) * 360) +90;
+	//turn seconds into base 100
+	const secondsDegrees = ((seconds/60) * 360) + 90;
+	//turn seconds into degrees 
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 	console.log(seconds);
+	const mins = now.getMinutes();
+	console.log('Minues: ' + mins);
+	const minsDegrees = ((mins/60) * 360) + 90;
+	minHand.style.transform = `rotate(${minsDegrees}deg)`;
+	const hour = now.getMinutes();
+	const hourDegrees = ((mins/12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setSeconds, 1000);
+// run set date function every second
+setInterval(setDate, 1000);

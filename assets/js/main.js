33d388078bb78f5c916008a41a881a1def6e5a7a
Quintessa <anderson.quintessa@gmail.com>
2019-01-09 19:26:12 -0600

    pushing javascript portion - second hand now working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
+const secondHand = document.querySelector('.second-hand');
+
 function setSeconds() {
 	const now = new Date();
 	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds/60) * 360) +90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 	console.log(seconds);
 }
 
